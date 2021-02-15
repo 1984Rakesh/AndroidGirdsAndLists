7da09e4e8db6635352e598a6c34cdbc915f70d71
Rakesh Patole <patolerakesh@gmail.com>
2021-02-15 15:02:28 +0530

    removed hardcoded categories , categories property is now optional

--- app/src/main/java/com/rakesh/gridactivity/GridAdapter.kt
+++ app/src/main/java/com/rakesh/gridactivity/GridAdapter.kt
@@ -1,40 +1,26 @@
 package com.rakesh.gridactivity
 
+import com.rakesh.model.Category
 import android.content.Context
-import android.content.res.Resources
-import android.media.Image
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 
 public class GridAdapter : BaseAdapter {
-    class CategoryItem(var name: String, var imageResId: Int) {
-    }
-
-    private val categoryItems : List<GridAdapter.CategoryItem> = mutableListOf(
-            CategoryItem("All",R.drawable.ic_icon_material_all_inclusive),
-            CategoryItem("Properties",R.drawable.ic_sale_house),
-            CategoryItem("Grocery",R.drawable.ic_grocery_basket),
-            CategoryItem("Home Care",R.drawable.ic_home_care),
-            CategoryItem("Furtniture & Garden",R.drawable.ic_furniture_and_garden),
-            CategoryItem("Motors",R.drawable.ic_motors),
-            CategoryItem("Mobile Phone & Devices",R.drawable.ic_mobile),
-            CategoryItem("Jobs",R.drawable.ic_job),
-            CategoryItem("Communinity",R.drawable.ic_community),
-    )
-
     private val context : Context
+    var categoryItems : MutableList<Category>? = null
+
 
     constructor(context: Context){
         this.context = context
     }
 
     override fun getCount(): Int {
-        return  categoryItems.size
+        return categoryItems?.size ?: 0
     }
 
     override fun getItem(position: Int): Any {
-        return categoryItems.get(position)
+        return categoryItems?.get(position)!!
     }
 
     override fun getItemId(position: Int): Long {
@@ -51,7 +37,7 @@ public class GridAdapter : BaseAdapter {
             gridItemView = GridItemView(context)
         }
 
-        var categoryItem : CategoryItem = getItem(position) as CategoryItem
+        var categoryItem : Category = getItem(position) as Category
         gridItemView.textView.text = categoryItem.name.toUpperCase()
         gridItemView.imageView.setImageResource(categoryItem.imageResId)
 
