e4c1f746ae2cbb518a0d33f3bfc7ab434717eec4
Rakesh Patole <patolerakesh@gmail.com>
2021-02-21 15:46:49 +0530

    added safe args plugin navigate from all categories to selected item details view

--- app/src/main/java/com/rakesh/view/CategoryItemListItemView.kt
+++ app/src/main/java/com/rakesh/view/CategoryItemListItemView.kt
@@ -2,9 +2,12 @@ package com.rakesh.view
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.navigation.findNavController
+import com.rakesh.fragment.CategoriesHomeFragmentDirections
 import com.rakesh.gridactivity.R
 
 class CategoryItemListItemView : LinearLayout {
@@ -27,5 +30,11 @@ class CategoryItemListItemView : LinearLayout {
         categoryItemImageView = findViewById(R.id.category_item_view_image)
 
         categoryItemImageView.setImageResource(R.drawable.placeholder_category_item)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val action = CategoriesHomeFragmentDirections.actionCategoriesHomeFragmentToCategoryItemDetailsFragment()
+        findNavController().navigate(action)
+        return super.onTouchEvent(event)
     }
 }
