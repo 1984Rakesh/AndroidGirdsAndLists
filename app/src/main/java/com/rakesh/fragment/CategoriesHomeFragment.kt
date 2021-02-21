40a8c38eb447ffe71f94056d7be30673224e2866
Rakesh Patole <patolerakesh@gmail.com>
2021-02-21 12:24:11 +0530

    added navigation component, added CategoriesHomeFragment that now main categories and top items in categories fragment

---
+++ app/src/main/java/com/rakesh/fragment/CategoriesHomeFragment.kt
@@ -0,0 +1,7 @@
+package com.rakesh.fragment
+
+import androidx.fragment.app.Fragment
+import com.rakesh.gridactivity.R
+
+class CategoriesHomeFragment : Fragment(R.layout.fragment_categories_home) {
+}
